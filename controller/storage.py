813da240757588cbfcfe683d9b181fed5e30bff6
HM <hieumdd@gmail.com>
2021-11-09 22:39:38 +0700

    feat: First commit

---
+++ controller/storage.py
@@ -0,0 +1,43 @@
+import io
+import csv
+from typing import Callable
+
+from google.cloud.storage import Client, Bucket, Blob
+
+INITIAL_BUCKET = ""
+SUCCESS_BUCKET = ""
+FAILED_BUCKET = ""
+
+
+def download_to_io(client: Client, blob: Blob) -> io.StringIO:
+    output = io.StringIO()
+    client.download_blob_to_file(blob, output)
+    return output
+
+
+def move_bucket(destination: str) -> Callable[[Client, Bucket, Blob], str]:
+    def move(client: Client, bucket: Bucket, blob: Blob) -> str:
+        bucket.copy_blob(
+            blob,
+            client.bucket(destination),
+            blob.name,
+        )
+        blob.delete()
+        return f"Moved {blob.name} to {destination}"
+
+    return move
+
+
+move_to_success = move_bucket(SUCCESS_BUCKET)
+move_to_failed = move_bucket(FAILED_BUCKET)
+
+
+def get_data(fileio: io.StringIO) -> list[dict]:
+    lines = fileio.read().splitlines()
+    return [
+        row
+        for row in csv.DictReader(
+            lines[3:],
+            fieldnames=[i.replace(" ", "_").lower() for i in lines[2].split(",")],
+        )
+    ]
---
+++ main.py
@@ -0,0 +1,57 @@
+from typing import TypedDict
+
+from google.cloud import bigquery, storage
+
+from controller.storage import download_to_io, move_to_success, move_to_failed, get_data
+from controller.bigquery import transform, load
+
+BQ_CLIENT = bigquery.Client()
+STORAGE_CLIENT = storage.Client()
+
+DATASET = ""
+TABLE = ""
+
+
+class Context:
+    event_id: str
+    event_type: str
+
+
+class Event(TypedDict):
+    bucket: str
+    name: str
+
+
+class Response(TypedDict, total=False):
+    status: str
+    output_rows: int
+    file_change: str
+
+
+def run(bucket: str, filename: str) -> Response:
+    source_bucket = STORAGE_CLIENT.bucket(bucket)
+    source_blob = source_bucket.blob(filename)
+    output = download_to_io(STORAGE_CLIENT, source_bucket, source_blob)
+    try:
+        output_rows = load(transform(get_data(output)), BQ_CLIENT, DATASET, TABLE)
+        return {
+            "status": "ok",
+            "output_rows": output_rows,
+            "file_change": move_to_success(STORAGE_CLIENT, source_blob),
+        }
+    except:
+        return {
+            "status": "failed",
+            "file_change": move_to_failed(STORAGE_CLIENT, source_blob),
+        }
+
+
+def main(event: Event, context: Context) -> Response:
+    if context.event_type != "google.storage.object.finalize":
+        response: Response = {
+            "status": "pass",
+        }
+    else:
+        response = run(event["bucket"], event["name"])
+    print(response)
+    return response
