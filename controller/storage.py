b4d7bc455fb1166b61288c724d6cec7f09c2b08c
HM <hieumdd@gmail.com>
2021-11-10 22:01:56 +0700

    feat: Added update & move mechanism

--- controller/storage.py
+++ controller/storage.py
@@ -1,5 +1,6 @@
 import io
 import csv
+from datetime import datetime
 
 from google.cloud.storage import Bucket, Blob
 
@@ -8,8 +9,14 @@ def download_to_io(blob: Blob) -> io.StringIO:
     return io.StringIO(blob.download_as_string().decode("utf-8"))
 
 
-def move_bucket(destination: Bucket, bucket: Bucket, blob: Blob) -> str:
-    bucket.copy_blob(blob, destination, blob.name)
+def move_bucket(destination: Bucket, bucket: Bucket, blob: Blob, now: datetime) -> str:
+    filename, ext = blob.name.split(".")
+    xx = f"{filename}-{now.strftime('%Y%m%d')}.{ext}"
+    bucket.copy_blob(
+        blob=blob,
+        destination_bucket=destination,
+        new_name=f"{filename}-{now.strftime('%Y%m%d')}.{ext}",
+    )
     blob.delete()
     return f"Moved {blob.name} to {destination}"
 
